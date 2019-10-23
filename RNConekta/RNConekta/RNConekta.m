ae4b4c608052f7cc8ada9a31b10fa2f7ea934f0f
Cesar Millan <cesarmillan16@gmail.com>
2019-10-23 12:05:43 -0500

    Yellow box warning "Module RNConekta  ... `init` but doesn't implement `requiresMainQueueSetup`."

--- RNConekta/RNConekta/RNConekta.m
+++ RNConekta/RNConekta/RNConekta.m
@@ -15,6 +15,16 @@
     Conekta *conekta;
     BOOL isCollected = NO;
 
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
++ (BOOL)requiresMainQueueSetup
+{
+    return YES;
+}
+
 - (id)init
 {
     UIViewController *controller = [[UIViewController alloc] init];
